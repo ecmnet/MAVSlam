cbec4c3b6428fcd2f58bce4f9fce4199ac6345d2
ecmnet <ecm@gmx.de>
2016-08-21 19:33:03 +0200

    Try API 4 and 5 for Realsense

--- MAVSlam/src/com/comino/realsense/boofcv/StreamRealSenseVisDepth.java
+++ MAVSlam/src/com/comino/realsense/boofcv/StreamRealSenseVisDepth.java
@@ -80,6 +80,8 @@ public class StreamRealSenseVisDepth {
 	public StreamRealSenseVisDepth(int devno , RealSenseInfo info)
 	{
 		ctx = LibRealSenseWrapper.INSTANCE.rs_create_context(5, error);
+		if(LibRealSenseWrapper.INSTANCE.rs_get_device_count(ctx, error)<1)
+			ctx = LibRealSenseWrapper.INSTANCE.rs_create_context(4, error);
 
 		if(LibRealSenseWrapper.INSTANCE.rs_get_device_count(ctx, error)<1) {
 			LibRealSenseWrapper.INSTANCE.rs_delete_context(ctx, error);
