6d55ea464eb660b649b84b342a193526d950eadf
ecmnet <ecm@gmx.de>
2016-08-23 18:26:12 +0200

    Rotation fixes, reset

--- MAVSlam/src/com/comino/realsense/boofcv/RealSenseMotionCapture.java
+++ MAVSlam/src/com/comino/realsense/boofcv/RealSenseMotionCapture.java
@@ -42,12 +42,13 @@ public class RealSenseMotionCapture {
 
 	private long tms=0;
 	private long framecount=0;
-	private float  initial_heading =0;
 
 	private DataModel model;
 
 	private boolean isRunning = false;
 	private IMAVMSPController control;
+
+	private float init_head_rad = 0;
 
 	public RealSenseMotionCapture(IMAVMSPController control) {
         this.control = control;
@@ -60,7 +61,7 @@ public class RealSenseMotionCapture {
 		configKlt.templateRadius = 3;
 
 		PointTrackerTwoPass<GrayU8> tracker =
-				FactoryPointTrackerTwoPass.klt(configKlt, new ConfigGeneralDetector(200, 2, 1),
+				FactoryPointTrackerTwoPass.klt(configKlt, new ConfigGeneralDetector(100, 2, 1),
 						GrayU8.class, GrayS16.class);
 
 		DepthSparse3D<GrayU16> sparseDepth = new DepthSparse3D.I<GrayU16>(1e-3);
@@ -78,14 +79,14 @@ public class RealSenseMotionCapture {
 
 		realsense.registerListener(new Listener() {
 
-			float fps; float dt; float md; int mf=0; int fpm; float dh;
+			float fps; float dt; float md; int mf=0; int fpm;
 
 			@Override
 			public void process(Planar<GrayU8> rgb, GrayU16 depth, long timeRgb, long timeDepth) {
-				framecount++;
-				dt = (timeDepth - oldTimeDepth)/1000f;
 
-				dh = MSPMathUtils.toRad(model.state.h) - initial_heading;
+				framecount++;
+
+				dt = (timeDepth - oldTimeDepth)/1000f;
 
 				if((System.currentTimeMillis() - tms) > 250) {
 					tms = System.currentTimeMillis();
@@ -107,16 +108,13 @@ public class RealSenseMotionCapture {
 
 				if(pos_raw_old!=null) {
 
-					//					float[] sr = MSPMathUtils.rotateRad((float)((pos_raw.x - pos_old.x)/dt),
-					//							         (float)((pos_raw.y - pos_old.y)/dt), -dh);
-
-					speed.x = (pos_raw.x - pos_raw_old.x)/dt;
-					speed.y = (pos_raw.z - pos_raw_old.z)/dt;
+					speed.y = (pos_raw.x - pos_raw_old.x)/dt;
+					speed.x = (pos_raw.z - pos_raw_old.z)/dt;
 					speed.z = (pos_raw.y - pos_raw_old.y)/dt;
 
 					if(Math.abs(speed.x)< 20 && Math.abs(speed.y)< 20 && Math.abs(speed.z)< 20 ) {
-						pos.x += -speed.x * dt;
-						pos.y += -speed.y * dt;
+						pos.x += speed.x * dt;
+						pos.y += speed.y * dt;
 						pos.z +=  speed.z * dt;
 					} else {
 						init();
@@ -124,24 +122,32 @@ public class RealSenseMotionCapture {
 					}
 				}
 
+				if(framecount < 10) {
+					float[] pos_rot = MSPMathUtils.rotateRad(model.state.l_x,model.state.l_y,init_head_rad);
+					pos.set(pos_rot[0],pos_rot[1], model.state.l_z);
+					return;
+				}
+
 				if(control!=null) {
+
+    				float[] pos_rot = MSPMathUtils.rotateRad((float)pos.x,(float)pos.y,-init_head_rad);
 
 					msg_vision_position_estimate sms = new msg_vision_position_estimate(1,1);
 					sms.usec =timeDepth*1000;
-					sms.x = (float) pos.x;
-					sms.y = (float) pos.y;
+					sms.x = (float) pos_rot[0];
+					sms.y = (float) pos_rot[1];
 					sms.z = (float) pos.z;
 					control.sendMAVLinkMessage(sms);
 
 
 					msg_msp_vision msg = new msg_msp_vision(1,2);
-					msg.x = (float) pos.x;
-					msg.y = (float) pos.y;
+					msg.x = (float) pos_rot[0];
+					msg.y = (float) pos_rot[1];
 					msg.z = (float) pos.z;
 					msg.vx = (float) speed.x;
 					msg.vy = (float) speed.y;
 					msg.vz = (float) speed.z;
-					msg.vh = dh;
+					msg.vh = model.state.h;
 					msg.fps = fps;
 					msg.flags = msg.flags | 1;
 					msg.tms = System.nanoTime() / 1000;
@@ -159,7 +165,6 @@ public class RealSenseMotionCapture {
 
 	public void start() {
 		isRunning = true;
-		initial_heading = MSPMathUtils.toRad(model.state.h);
 		init();
 		realsense.start();
 	}
@@ -169,11 +174,10 @@ public class RealSenseMotionCapture {
 	}
 
 	private void init() {
+		init_head_rad = MSPMathUtils.toRad(model.state.h);
 		control.writeLogMessage(new LogMessage("[vis] reset odometry",MAV_SEVERITY.MAV_SEVERITY_INFO));
 		visualOdometry.reset();
 		pos_raw = null; pos_raw_old = null; speed.x=0; speed.y=0; speed.z=0;
-		initial_heading = MSPMathUtils.toRad(model.state.h);
-		pos.set(model.state.l_x,model.state.l_y, model.state.l_z);
 		framecount = 0;
 	}
 
