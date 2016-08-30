0bb86109812601e1b907a4dca5d42221a07a4530
ecmnet <ecm@gmx.de>
2016-08-30 13:24:48 +0200

    RealSensePositionEstimator enhanced

--- MAVSlam/src/com/comino/realsense/boofcv/RealSensePositionEstimator.java
+++ MAVSlam/src/com/comino/realsense/boofcv/RealSensePositionEstimator.java
@@ -63,9 +63,10 @@ import georegression.struct.se.Se3_F64;
 
 public class RealSensePositionEstimator {
 
-	private static final int MIN_COUNT   = 10;
-	private static final int MIN_QUALITY = 20;
-	private static final int MAXTRACKS   = 120;
+	private static final int    INIT_TIME_MS   = 600;
+	private static final float  MAX_SPEED   	= 20;
+	private static final int    MIN_QUALITY 	= 10;
+	private static final int    MAXTRACKS   	= 120;
 
 	private StreamRealSenseVisDepth realsense;
 	private RealSenseInfo info;
@@ -78,8 +79,8 @@ public class RealSensePositionEstimator {
 	private Vector3D_F64 speed   = new Vector3D_F64();
 	private Vector3D_F64 pos     = new Vector3D_F64();
 
-	private long tms=0;
-	private long framecount=0;
+	private long fps_tms   =0;
+	private long init_tms  =0;
 
 	private DataModel model;
 
@@ -126,12 +127,10 @@ public class RealSensePositionEstimator {
 			@Override
 			public void process(Planar<GrayU8> rgb, GrayU16 depth, long timeRgb, long timeDepth) {
 
-				framecount++;
-
 				dt = (timeDepth - oldTimeDepth)/1000f;
 
-				if((System.currentTimeMillis() - tms) > 250) {
-					tms = System.currentTimeMillis();
+				if((System.currentTimeMillis() - fps_tms) > 250) {
+					fps_tms = System.currentTimeMillis();
 					if(mf>0)
 						fps = fpm/mf;
 					mf=0; fpm=0;
@@ -148,51 +147,52 @@ public class RealSensePositionEstimator {
 				Se3_F64 leftToWorld = visualOdometry.getCameraToWorld();
 				pos_raw = leftToWorld.getT();
 
+				quality = visualOdometry.getInlierCount() *100 / MAXTRACKS;
 
 				if(pos_raw_old!=null) {
+
 
 					speed.y = (pos_raw.x - pos_raw_old.x)/dt;
 					speed.x = (pos_raw.z - pos_raw_old.z)/dt;
 					speed.z = (pos_raw.y - pos_raw_old.y)/dt;
 
-					if(Math.abs(speed.x)< 20 && Math.abs(speed.y)< 20 && Math.abs(speed.z)< 20 ) {
-						pos.x += speed.x * dt;
-						pos.y += speed.y * dt;
+					if(Math.abs(speed.x)< MAX_SPEED && Math.abs(speed.y)< MAX_SPEED && Math.abs(speed.z)< MAX_SPEED
+							&& quality > MIN_QUALITY ) {
+
+						// TODO: eventually rotate from body to nav frame in PX4
+						MSPMathUtils.rotateRad(pos_rot,(float)speed.x * dt,(float)speed.y * dt,-init_head_rad);
+
+						pos.x += pos_rot[0];
+						pos.y += pos_rot[1];
+
+						// TODO: PITCH, ROLL correction
 						pos.z += speed.z * dt;
+
 					} else {
-						init();
 						return;
 					}
 				}
 
-				quality = visualOdometry.getInlierCount() *100 / MAXTRACKS;
+				pos_raw_old = pos_raw.copy();
 
-				if(framecount < MIN_COUNT) {
-					MSPMathUtils.rotateRad(pos_rot, model.state.l_x,model.state.l_y,init_head_rad);
-					pos.set(pos_rot[0],pos_rot[1], model.raw.di);
+				if((System.currentTimeMillis()-init_tms) < INIT_TIME_MS) {
+					pos.set(model.state.l_x,model.state.l_y, model.raw.di);
 					return;
-				} else {
-					if(quality < MIN_QUALITY) {
-						init();
-					    return;
-					}
 				}
 
 				if(control!=null) {
 
-    				MSPMathUtils.rotateRad(pos_rot,(float)pos.x,(float)pos.y,-init_head_rad);
-
 					msg_vision_position_estimate sms = new msg_vision_position_estimate(1,1);
 					sms.usec =timeDepth*1000;
-					sms.x = (float) pos_rot[0];
-					sms.y = (float) pos_rot[1];
+					sms.x = (float) pos.x;
+					sms.y = (float) pos.y;
 					sms.z = (float) pos.z;
 					control.sendMAVLinkMessage(sms);
 
 
 					msg_msp_vision msg = new msg_msp_vision(1,2);
-					msg.x = (float) pos_rot[0];
-					msg.y = (float) pos_rot[1];
+					msg.x = (float) pos.x;
+					msg.y = (float) pos.y;
 					msg.z = (float) pos.z;
 					msg.vx = (float) speed.x;
 					msg.vy = (float) speed.y;
@@ -205,7 +205,6 @@ public class RealSensePositionEstimator {
 					control.sendMAVLinkMessage(msg);
 				}
 
-				pos_raw_old = pos_raw.copy();
 			}
 		});
 	}
@@ -226,17 +225,14 @@ public class RealSensePositionEstimator {
 	}
 
 	private void init() {
-		init_head_rad = MSPMathUtils.toRad(model.state.h);
-		control.writeLogMessage(new LogMessage("[vis] reset odometry",MAV_SEVERITY.MAV_SEVERITY_INFO));
+		if((System.currentTimeMillis()-init_tms)>INIT_TIME_MS)
+		   control.writeLogMessage(new LogMessage("[vis] reset odometry",
+				    MAV_SEVERITY.MAV_SEVERITY_WARNING));
 		visualOdometry.reset();
-		pos_raw = null; pos_raw_old = null; speed.x=0; speed.y=0; speed.z=0;
-		framecount = 0;
+		init_head_rad = MSPMathUtils.toRad(model.state.h);
+		pos.set(model.state.l_x,model.state.l_y, model.raw.di);
+		init_tms = System.currentTimeMillis();
 	}
-
-	private void collisionDetection() {
-
-	}
-
 
 	public static void main(String[] args) {
 		new RealSensePositionEstimator();
