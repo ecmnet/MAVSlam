78019b96f50337c556178dfdb9b392d05baa87fc
ecmnet <ecm@gmx.de>
2018-09-19 15:02:02 +0200

    make use of msg_odometry (WIP)

--- MAVSlam/src/com/comino/slam/estimators/MAVVisualPositionEstimator.java
+++ MAVSlam/src/com/comino/slam/estimators/MAVVisualPositionEstimator.java
@@ -579,8 +579,29 @@ public class MAVVisualPositionEstimator implements IPositionEstimator {
 
 			odometry.pose_covariance[0] = Float.NaN;
 
-			//TODO: Fill other info from here...
+			if( do_xy_speed) {
+				odometry.vx = (float) speed_ned.T.z;
+				odometry.vy = (float) speed_ned.T.x;
+			} else {
+				odometry.vx = Float.NaN;
+				odometry.vy = Float.NaN;
+			}
 
+			if(do_z_position) {
+				odometry.vz = (float) pos_ned.T.y;
+			} else {
+				odometry.vz = Float.NaN;
+			}
+
+			odometry.twist_covariance[0] = Float.NaN;
+
+			//TODO: Fill quaternions
+			// odometry.q[]
+
+			//TODO: Determine and fill attitude speeds
+			//odometry.pitchspeed
+			//odometry.rollspeed
+			//odometry.yawspeed
 
 			control.sendMAVLinkMessage(odometry);
 
@@ -590,40 +611,40 @@ public class MAVVisualPositionEstimator implements IPositionEstimator {
 
 
 
-//	private void publishPX4Vision() {
-//		if(do_odometry && (System.currentTimeMillis()-last_pos_tms) > PUBLISH_RATE_PX4) {
-//			last_pos_tms = System.currentTimeMillis();
-//
-//			msg_vision_position_estimate sms = new msg_vision_position_estimate(1,2);
-//			sms.usec = (long)estTimeDepth_us;
-//			if(do_xy_position)  {
-//				sms.x = (float) pos_ned.T.z;
-//				sms.y = (float) pos_ned.T.x;
-//			} else {
-//				sms.x = Float.NaN;
-//				sms.y = Float.NaN;
-//			}
-//
-//			if(do_z_position) {
-//				sms.z = (float) pos_ned.T.y;
-//			} else {
-//				sms.z = Float.NaN;
-//			}
-//
-//			if(do_attitude) {
-//				sms.roll  = (float)visAttitude[0];
-//				sms.pitch = (float)visAttitude[1];
-//				sms.yaw   = (float)visAttitude[2];
-//			}
-//
-//			sms.covariance[0] = Float.NaN;
-//
-//			control.sendMAVLinkMessage(sms);
-//
-//			model.sys.setSensor(Status.MSP_OPCV_AVAILABILITY, true);
-//
-//		}
-//	}
+	//	private void publishPX4Vision() {
+	//		if(do_odometry && (System.currentTimeMillis()-last_pos_tms) > PUBLISH_RATE_PX4) {
+	//			last_pos_tms = System.currentTimeMillis();
+	//
+	//			msg_vision_position_estimate sms = new msg_vision_position_estimate(1,2);
+	//			sms.usec = (long)estTimeDepth_us;
+	//			if(do_xy_position)  {
+	//				sms.x = (float) pos_ned.T.z;
+	//				sms.y = (float) pos_ned.T.x;
+	//			} else {
+	//				sms.x = Float.NaN;
+	//				sms.y = Float.NaN;
+	//			}
+	//
+	//			if(do_z_position) {
+	//				sms.z = (float) pos_ned.T.y;
+	//			} else {
+	//				sms.z = Float.NaN;
+	//			}
+	//
+	//			if(do_attitude) {
+	//				sms.roll  = (float)visAttitude[0];
+	//				sms.pitch = (float)visAttitude[1];
+	//				sms.yaw   = (float)visAttitude[2];
+	//			}
+	//
+	//			sms.covariance[0] = Float.NaN;
+	//
+	//			control.sendMAVLinkMessage(sms);
+	//
+	//			model.sys.setSensor(Status.MSP_OPCV_AVAILABILITY, true);
+	//
+	//		}
+	//	}
 
 	private void publisMSPVision() {
 		if((System.currentTimeMillis()-last_msp_tms) > PUBLISH_RATE_MSP) {
